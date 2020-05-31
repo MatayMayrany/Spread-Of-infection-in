ab04ef216e14f4817ff34ca83b2a86a4f731c811
Matay Mayrany <mayrany@kth.se>
2020-05-31 19:50:38 +0200

    yessssir

---
+++ StatusType.java
@@ -0,0 +1,7 @@
+package company;
+public class StatusType {
+    public static String HEALTHY = " h ";
+    public static String INFECTED = "[I]";
+    public static String DEAD = "[X]";
+    public static String RECOVERED = "[R]";
+}
